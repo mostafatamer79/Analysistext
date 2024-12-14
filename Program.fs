c5e15ddc98c4052176c33b6a28246a2ce3fec6d9
mostafatam79 <105394598+mostafatamer79@users.noreply.github.com>
2024-12-14 16:50:44 +0200

    cleaning the text to be number and space and character and # only

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.Text.RegularExpressions
+
+let cleanText (text: string) =
+    let cleaned = Regex.Replace(text, "[^\w\s#]", "") 
+    cleaned
+
+[<EntryPoint>]
+let main argv =
+    let inputText = "Hello World!"
+
+    let cleanedText = cleanText inputText
+
+    printfn "Original text: %s" inputText
+    printfn "Cleaned text: %s" cleanedText
+
+    0
