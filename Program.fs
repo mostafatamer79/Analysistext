153ef9558a8b8b27ddd7ec07cff9985f3bd3b0e2
MohraMaged <120310772+MohraMaged@users.noreply.github.com>
2024-12-15 14:32:46 +0200

    create the main function
    create function count the word, count the sentence, and the paragraph, and calculate the word frequency, and the average of sentence length

--- Program.fs
+++ Program.fs
@@ -27,7 +27,34 @@ let getTextInput (filePath: string) =
     | ex -> 
         MessageBox.Show($"Unknown error: {ex.Message}") |> ignore
         ""
+let analyzeText (text: string) =
+    let cleanedText = cleanText text
+    let words = cleanedText.Split([| ' '; '\n'; '\t' |], StringSplitOptions.RemoveEmptyEntries)
+    
 
+    let sentences = Regex.Split(text, "(?<=[.!?])\s+")
+    let filteredSentences = sentences |> Array.filter (fun s -> s.Trim().Length > 0)
+
+    let paragraphs = text.Split([| '\n' |], StringSplitOptions.RemoveEmptyEntries)
+
+    let wordCount = words.Length
+    let sentenceCount = filteredSentences.Length
+    let paragraphCount = paragraphs.Length
+
+    let wordFrequency =
+        words
+        |> Seq.map (fun word -> word.ToLowerInvariant())
+        |> Seq.countBy id
+        |> Seq.sortByDescending snd
+
+    let averageSentenceLength =
+        if sentenceCount > 0 then
+            float wordCount / float sentenceCount
+        else
+            0.0
+
+   
+    wordCount, sentenceCount, paragraphCount, wordFrequency, averageSentenceLength
 [<EntryPoint>]
 let main argv =
     let filePath = "test.txt"
